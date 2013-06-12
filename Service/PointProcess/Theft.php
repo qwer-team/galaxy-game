4219fa0d9e03738ff6f19deb5cc5afce8f8048ec
minout <minouts@gmail.com>
2013-06-12 19:10:36 +0300

    _

---
+++ Service/PointProcess/Theft.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Galaxy\GameBundle\Service\PointProcess;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Galaxy\GameBundle\Service\PointProcess\PointTypeProcess;
+
+class Theft extends ContainerAware implements PointTypeProcess
+{
+    public function proceed($response, $userId)
+    {
+        $userInfo = $this->getUserInfo($userId);
+        $parameter = $response['subtype']['parameter'];
+        $flipperId = $userInfo->getFlipper()->getId();
+        $newFlipperId = $flipperId - $parameter;
+        $newFlipperId = ($newFlipperId >= 1 ? $newFlipperId : 1); 
+        if($newFlipperId != $flipperId){
+            $newFlipper = $this->getNewFlipper($newFlipperId);
+            $userInfo->setFlipper($newFlipper);
+        }
+        
+        $this->container->get("doctrine.orm.entity_manager")->flush();
+    }
+    
+    /**
+     * 
+     * @param type $userId
+     * @return \Galaxy\GameBundle\Entity\UserInfo
+     */
+    private function getUserInfo($userId)
+    {
+        $em = $this->container->get("doctrine.orm.entity_manager");
+        $namespace = "GalaxyGameBundle:UserInfo";
+        $repo = $em->getRepository($namespace);
+
+        return $repo->findOneByUserId($userId);
+    }
+    
+    
+    /**
+     * 
+     * @param type $flipperId
+     * @return \Galaxy\GameBundle\Entity\Flipper
+     */
+    private function getNewFlipper($flipperId)
+    {
+        $em = $this->container->get("doctrine.orm.entity_manager");
+        $namespace = "GalaxyGameBundle:Flipper";
+        $repo = $em->getRepository($namespace);
+
+        return $repo->find($flipperId);
+    }
+    
+}
